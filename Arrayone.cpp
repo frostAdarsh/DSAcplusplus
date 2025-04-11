79d7f3004d518a06e30f2b1ff2971125ab0d1d71
frostAdarsh <adarshmani017@gmail.com>
2025-04-11 20:39:35 +0530

    4

--- Arrayone.cpp
+++ Arrayone.cpp
@@ -44,37 +44,59 @@ int main()
 
     //---------Search in array
 
-   
-        // int arr[] = {41, 22, 33, 14, 45, 46, 78};
-        // int n = sizeof(arr) / sizeof(arr[0]);
-        // int x;
-        // cout << "Enter the element to search: ";
-        // cin >> x;
-    
-        // for(int i = 0; i < n; i++) {
-        //     if(arr[i] == x) {
-        //         cout << "Element found at index: " << i << endl;
-        //         return 0; 
-        //     }
-        // }
-    
-        // cout << "Element not found" << endl;
-        // return -1;
-    
+    // int arr[] = {41, 22, 33, 14, 45, 46, 78};
+    // int n = sizeof(arr) / sizeof(arr[0]);
+    // int x;
+    // cout << "Enter the element to search: ";
+    // cin >> x;
 
-        //---------Reverse an array
+    // for(int i = 0; i < n; i++) {
+    //     if(arr[i] == x) {
+    //         cout << "Element found at index: " << i << endl;
+    //         return 0;
+    //     }
+    // }
 
-        int arr[] = {1, 2, 3, 4, 5};
-        int start = 0;
-        int end = sizeof(arr)/sizeof(arr[0]) - 1;
-        
-        while(start<end){
-            swap(arr[start], arr[end]);
-            start++;
-            end--;
+    // cout << "Element not found" << endl;
+    // return -1;
+
+    //---------Reverse an array
+
+    // int arr[] = {1, 2, 3, 4, 5};
+    // int start = 0;
+    // int end = sizeof(arr)/sizeof(arr[0]) - 1;
+
+    // while(start<end){
+    //     swap(arr[start], arr[end]);
+    //     start++;
+    //     end--;
+    // }
+
+    // for(int i=0;i<sizeof(arr)/sizeof(arr[0]);i++){
+    //     cout<<arr[i]<<" ";
+    // }
+
+    //------------Second largest element in array
+    int arr[] = {1, 2, 3, 4, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int ans = INT8_MIN;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] > ans)
+        {
+            ans = arr[i];
         }
+    }
 
-        for(int i=0;i<sizeof(arr)/sizeof(arr[0]);i++){
-            cout<<arr[i]<<" ";
+    int second = INT8_MIN;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] != ans)
+        {
+            second = max(second, arr[i]);
         }
+    }
+    cout << "The second largest element is: " << second << endl;
+    return 0;
 }
